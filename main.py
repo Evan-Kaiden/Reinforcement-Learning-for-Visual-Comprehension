0bbc17254288a10761297c3974a083ccd79a3ff9
Evan Kaiden <evankaiden@Evans-Laptop.local>
2025-08-01 18:37:21 -0700

    running REINFORCE algo

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from dataset import trainloader, testloader
 
-from glimpse_agent import glimpseAgent
-from networks import PerceptionPolicy, PerceptionEncoder, ContextMemory, Classifier
-import torch
+from glimpse_agent import GlimpseAgent
+from networks import PerceptionPolicy, PerceptionEncoder, ContextMemory, Classifier, gate
+
+"""Reward. at each step pass the current context to the classifier. if the currrent context has pused the prob distribition
+towards closer towards the target class, then reward is positive, else negative. final reward is the sum of all rewards plus the final classification reward."""
+
 def main():
-    patch = 12
-    embd = 128
+    patch = 6
+    embd = 256
     classes = 10
-    img_size = 32
-    img_channels = 3
-    policy = PerceptionPolicy(embd)
-    encoder = PerceptionEncoder(img_channels, patch, embd)
-    memory = ContextMemory(embd)
-    classifier = Classifier(embd, classes)
-    torch.autograd.set_detect_anomaly(True)
-    agent = glimpseAgent(policy, encoder, memory, classifier, img_size, patch, 1e-3, 1e-3)
-    agent.train(trainloader)
+    img_size = 28
+    img_channels = 1
+
+    policy = PerceptionPolicy(img_size*img_size, embd)
+    encoder = PerceptionEncoder(input_shape=patch, embd_dim=embd, in_channels=img_channels)
+    classifier = Classifier(embd_dim=embd, n_classes=classes)
+    attention_gate = gate(embd_dim=embd)
+
+    agent = GlimpseAgent(policy=policy, encoder=encoder,  classifier=classifier, gate=attention_gate,
+                         image_size=img_size, patch_size=patch, embd_dim=embd,  device=None)
+
+    agent.train_agent(epochs=20, trainloader=trainloader, testloader=testloader)
 
 if __name__ == '__main__':
     main()
