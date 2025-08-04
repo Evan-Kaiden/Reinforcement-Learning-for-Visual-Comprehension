18952e151d2fa24f27439f09208d7b4566e203a3
Evan Kaiden <evankaiden@Evans-Laptop.local>
2025-08-03 19:38:17 -0700

    added short funciton to map from range [-1,1] to any range 0, n

--- utils.py
+++ utils.py
@@ -1,14 +1,19 @@
 import torch
- 
-def make_glimpse_grid(center, g, S, align_corners=True):
+
+
+def map_scale(val, img_size):
+    val = ((val + 1) / 2) * img_size
+    return val
+
+def make_glimpse_grid(center, p, S, align_corners=True):
     # center: [B,2] in [-1,1] (x,y), return [B,p,p,2]
     B = center.size(0)
-    lin = torch.linspace(-1, 1, g, device=center.device, dtype=center.dtype)
+    lin = torch.linspace(-1, 1, p, device=center.device, dtype=center.dtype)
     gy, gx = torch.meshgrid(lin, lin, indexing='ij')
     base = torch.stack([gx, gy], dim=-1).unsqueeze(0).expand(B, -1, -1, -1)
     if align_corners:
-        scale = (g - 1) / max(S - 1, 1)
+        scale = (p - 1) / max(S - 1, 1)
     else:
-        scale = g / S
+        scale = p / S
     grid = center.view(B, 1, 1, 2) + scale * base
     return grid.clamp_(-1, 1)
