fe8edd7967eab2f9e32e0c655b57088cb2a9e3a2
Evan Kaiden <evankaiden@Evans-Laptop.local>
2025-08-08 00:39:46 -0700

    added entropy scaler

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import torch
 
+def entropy_weight_t(n, total, start_entropy):
+    """scale linearly from start_entropy -> 0 over T timesteps"""
+    return start_entropy * (1 - n / total)
 
 def map_scale(val, img_size):
     """Maps from range [-1, 1] to range [0, img_size]"""
@@ -7,14 +10,20 @@ def map_scale(val, img_size):
     return val
 
 def make_glimpse_grid(center, p, S, align_corners=True):
-    # center: [B,2] in [-1,1] (x,y), return [B,p,p,2]
+    """Returns a grid [B,p,p,2] a sampling grid that maps [-1,1] to a location"""
     B = center.size(0)
-    lin = torch.linspace(-1, 1, p, device=center.device, dtype=center.dtype)
+
+    # Create Grid
+    lin    = torch.linspace(-1, 1, p, device=center.device, dtype=center.dtype)
     gy, gx = torch.meshgrid(lin, lin, indexing='ij')
     base = torch.stack([gx, gy], dim=-1).unsqueeze(0).expand(B, -1, -1, -1)
+
+    # Create Value Scale
     if align_corners:
         scale = (p - 1) / max(S - 1, 1)
     else:
         scale = p / S
+
+    # Scale Grid
     grid = center.view(B, 1, 1, 2) + scale * base
     return grid.clamp_(-1, 1)
